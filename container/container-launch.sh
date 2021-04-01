8d0f397ac5c2edb5774aa8ccead702072e83fc40
Alex Flom <aflom@redhat.com>
2021-03-31 20:17:48 -0400

    wrong tag

--- container/container-launch.sh
+++ container/container-launch.sh
@@ -5,5 +5,5 @@ podman run \
   -v ./:/host   -v /dev/fuse:/dev/fuse:rw \
   -v operators:/var/lib/containers:rw \
   --rm --ulimit host --privileged \
-  quay.io/redhatgov/operator-mirror:offline-dev $@
+  quay.io/redhatgov/operator-mirror:latest $@
 
